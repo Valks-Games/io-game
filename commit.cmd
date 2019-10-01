cfb9b5d3702a70680b687eab51cccb2d6ae9bbd3
valkyrienyanko <sebastianbelle074@gmail.com>
2019-10-01 13:17:02 -0500

    Test Commit

--- commit.cmd
+++ commit.cmd
@@ -1,6 +1,7 @@
 @ECHO OFF
+set /p message="Commit Message: "
 call git add .
-call git commit -m "Update"
+call git commit -m "%message%"
 call git pull origin master
 call git push -u origin master
 PAUSE
