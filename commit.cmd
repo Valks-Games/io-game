cd4040e59283f9d7cdb3cd098414108ed6848c0a
valkyrienyanko <sebastianbelle074@gmail.com>
2019-10-01 13:24:20 -0500

    Test 3

--- commit.cmd
+++ commit.cmd
@@ -1,9 +1,17 @@
+::Automating commits.
 @ECHO OFF
 :commit
+::Message of commit we are sending.
 set /p message="Commit Message: "
 call git add .
 call git commit -m "%message%"
+::Should we pull the origin master?
+set /P c=Pull origin master? (Y/n): 
+if /I "%c%" EQU "Y" goto :pull
+if /I "%c%" EQU "N" goto :nopull
+:pull
 call git pull origin master
+:nopull
 call git push -u origin master
 set /P c=Send another commit? (Y/n): 
 if /I "%c%" EQU "Y" goto :commit
