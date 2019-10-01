5b6281c858c9e02de259f71821c0f96ad2440d69
valkyrienyanko <sebastianbelle074@gmail.com>
2019-10-01 13:19:12 -0500

    Test

--- commit.cmd
+++ commit.cmd
@@ -1,7 +1,11 @@
 @ECHO OFF
+:commit
 set /p message="Commit Message: "
 call git add .
 call git commit -m "%message%"
 call git pull origin master
 call git push -u origin master
-PAUSE
+set /P c=Send another commit? (Y/n): 
+if /I "%c%" EQU "Y" goto :commit
+if /I "%c%" EQU "N" goto :next
+:next
