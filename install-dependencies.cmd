b5dfa22ef3246c536d11ae9df6849ddf3d44690b
valkyrienyanko <sebastianbelle074@gmail.com>
2019-10-01 16:17:38 -0500

    Update install-dependencies.cmd

--- install-dependencies.cmd
+++ install-dependencies.cmd
@@ -1,9 +1,9 @@
 @ECHO OFF
 title Install Dependencies
 mode con: cols=120 lines=30
-cls
+
 ::Express
-set /P c=Express is required for running the server. [1mInstall express?[0m (Y/n): 
+set /P c=Express is required for running the server. Install express? (Y/n): 
 if /I "%c%" EQU "Y" goto :install
 if /I "%c%" EQU "N" goto :next
 :install
@@ -12,7 +12,7 @@ call npm i express
 :next
 
 ::Socket.io
-set /P c=Socket.io is required to connect to the server. [1mInstall socket.io?[0m (Y/n): 
+set /P c=Socket.io is required to connect to the server. Install socket.io? (Y/n): 
 if /I "%c%" EQU "Y" goto :install
 if /I "%c%" EQU "N" goto :next
 :install
@@ -21,7 +21,7 @@ call npm i socket.io
 :next
 
 ::Nodemon
-set /P c=Nodemon will automatically restart the server whenever file changes are detected. [1mInstall nodemon?[0m (Y/n): 
+set /P c=Nodemon will automatically restart the server whenever file changes are detected. Install nodemon? (Y/n): 
 if /I "%c%" EQU "Y" goto :install
 if /I "%c%" EQU "N" goto :next
 :install
@@ -30,7 +30,7 @@ call npm i nodemon -g
 :next
 
 ::Electron
-set /P c=Electron is required for using and building electron based applications. [1mInstall electron?[0m (Y/n): 
+set /P c=Electron is required for using and building electron based applications. Install electron? (Y/n): 
 if /I "%c%" EQU "Y" goto :install
 if /I "%c%" EQU "N" goto :next
 :install
